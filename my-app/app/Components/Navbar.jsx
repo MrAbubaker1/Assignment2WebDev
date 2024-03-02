f57e7eb7f42caf420f00f7123f1a65e9ab9ca383
Amadou <amadouamos@hotmail.com>
2024-03-02 13:35:16 -0700

    first commit

---
+++ my-app/app/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto text-white">
+        <h1 className="text-2xl font-bold">SAIT Student Portal</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
