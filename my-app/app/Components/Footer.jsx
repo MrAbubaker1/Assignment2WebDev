a3df22c4f2db7f60af00c0f511d9c9477bddc13f
Amadou <amadouamos@hotmail.com>
2024-03-03 17:38:39 -0700

    adding current date to footer

--- my-app/app/Components/Footer.jsx
+++ my-app/app/Components/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const Footer = () => {
+  const getCurrentDate = () => {
+    const currentDate = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return currentDate.toLocaleDateString('en-US', options);
+  };
+
   return (
     <footer className="bg-blue-500 p-4 mt-8 text-2xl text-white text-center font-bold">
       <div className="container mx-auto text-center">
-        <p>Groups 9 & 12 March 12, 2024</p>
+        <p>New Generation High School Admin Portal {getCurrentDate()}</p>
       </div>
     </footer>
   );
